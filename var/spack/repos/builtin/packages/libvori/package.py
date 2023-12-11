32e4f81c1746fda8118da26709f6052dba03ceae
yizeyi18 <18586016708@163.com>
2023-12-11 19:49:01 +0800

    add '+pic' variant to solve https://github.com/spack/spack/issues/41445#issuecomment-1845536752

--- var/spack/repos/builtin/packages/libvori/package.py
+++ var/spack/repos/builtin/packages/libvori/package.py
@@ -19,3 +19,13 @@ class Libvori(CMakePackage):
     version("201229", sha256="da0afb292c94f8de2aaebfd0b692d15ffd86083cb8a48478b07ca93823decc06")
     version("201224", sha256="16f6c49eaa17ea23868925dbaae2eca71bdacbe50418c97d6c55e05728038f31")
     version("201217", sha256="6ad456ed6ca5d28cadcc0d90eabe8fff5caa77b99f12764323de5e3ae21cddf5")
+
+    variant("pic", default=False, description="Build position independent code")
+
+    def cmake_args(self):
+        options = []
+
+        if self.spec.satisfies("+pic"):
+            option.sppend(self.define("CMAKE_C_FLAGS", self.compiler.cc_pic_flag))
+            
+        return options
