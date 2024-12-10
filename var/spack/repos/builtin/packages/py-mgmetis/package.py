855943ff2928dc748789a25e50770667ada4aa2a
teddy <teddy.chantrait@gmail.com>
2024-12-10 03:29:59 +0100

    py-mgmetis: remove constrains 3.X for mpi4py & 1.X for numpy depandancies (#47890)
    Co-authored-by: t. chantrait <teddy.chantrait@cea.fr>

--- var/spack/repos/builtin/packages/py-mgmetis/package.py
+++ var/spack/repos/builtin/packages/py-mgmetis/package.py
@@ -19,8 +19,8 @@ class PyMgmetis(PythonPackage):
 
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("py-numpy@1.20.0:1.26.4", type=("build", "run"))
+    depends_on("py-numpy@1.20.0:", type=("build", "run"))
     depends_on("py-cython", type=("build"))
-    depends_on("py-mpi4py@3.0.3:3", type=("build", "run"))
+    depends_on("py-mpi4py@3.0.3:", type=("build", "run"))
     depends_on("py-pytest")
     depends_on("metis+shared", type="all")
